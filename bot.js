99b01ee7c050ab1ebb945358c0bcaacd8527e81c
Maltaran <61850493+Maltaran@users.noreply.github.com>
2022-06-15 19:18:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,7 +86,7 @@ bot.on('message', (message) => {
     lastpts = message.author.tag;
     var names = {"xd16z069":"8 Ball","kwj3kezd":"Adrenaline","xd16z569":"Art attack","xd0le71d":"Well's Bells","o9xky21w":"Bob's leg","ewpe2qz9":"Cannon man","z98eojl9":"Kick'em all","r9grm3kw":"Sweet tooth","xd02p7j9":"Downtown any%","495yq8jw":"Downtown 100%","rw6r7v7w":"Black out","ywenv2rw":"Fast Food","rdqgm119":"Fortified race","29vj8kn9":"Stronghold","kwj3kyzd":"Gone with the Wind","5wk8mrqw":"The persuaders","rw6r7n7w":"Half pipe","gdrg0jzw":"Ice scream","69zxlrod":"Last and furious","ywenv4rw":"Life on the edge","xd4n53pw":"Mona racer","xd4n56pw":"My balloons","kwj3k31d":"My way","o9xkyl1w":"Par 5","z98eo8l9":"Penguin carnage","nwlnm6rd":"Time attack","y9mymp19":"Rock and Race","owolmkyw":"Table Royale","xd0le01d":"Sugar rush","495yqjjw":"Heavy croquet","59236jrd":"Speed racer","59236ord":"From the sky","5d7lj7v9":"Foosball","n938x0ew":"Tea time","5d7lj3v9":"Thin line","rdqgm019":"Room for dessert","rdnrm5vw":"Titanic","ewpe2pz9":"Toy Race","nwlnmgrd":"Training day","29vj8yn9":"Toygara falls","y9myme19":"Wax on","ldyjrezd":"White soccer","n938xgew":"Cold rush","rdnrmpvw":"On the rocks","owolm2yw":"Gravity zero","z27l91zd":"Full game [ANY%]","rklqzvw2":"Full game [100%]","ndx9l8od":"All Antarctic","jdz7vzrk":"All Powerups","5dw7vnnk":"All Battle Flags"};
     var tracks = {};
-    var users = {"qjo0wmnj":{name:"Maltaran",points:0,maps:[]},"o86w6o5x":{name:"kiraa96",points:0,maps:[]},"zx7dqovx":{name:"jeryrep",points:0,maps:[]},"0jmrk1n8":{name:"alexinho.k",points:0,maps:[]},"pj025qw8":{name:"Ikdro",points:0,maps:[]},"pj0kz2r8":{name:"Depresivie",points:0,maps:[]},"81409038":{name:"Oleksik_PL",points:0,maps:[]},"8l0reo78":{name:"TwinTurbo122",points:0,maps:[]},"j5wre7wj":{name:"Hawk0817",points:0,maps:[]},"xz9k6w08":{name:"PlayerOne",points:0,maps:[]},"j96edknj":{name:"Raszta55",points:0,maps:[]},"8qr4erqj":{name:"xKubSon",points:0,maps:[]},"8grg39yx":{name:"uZer",points:0,maps:[]},"j26qnmnx":{name:"MadTrackEnthusiast",points:0,maps:[]},"18q0nq7j":{name:"luigi100",points:0,maps:[]}};
+    var users = {"qjo0wmnj":{name:"Maltaran",points:0,maps:[]},"o86w6o5x":{name:"kiraa96",points:0,maps:[]},"zx7dqovx":{name:"jeryrep",points:0,maps:[]},"0jmrk1n8":{name:"alexinho.k",points:0,maps:[]},"pj025qw8":{name:"Ikdro",points:0,maps:[]},"pj0kz2r8":{name:"Depresivie",points:0,maps:[]},"81409038":{name:"Oleksik_PL",points:0,maps:[]},"8l0reo78":{name:"TwinTurbo122",points:0,maps:[]},"j5wre7wj":{name:"Hawk0817",points:0,maps:[]},"xz9k6w08":{name:"PlayerOne",points:0,maps:[]},"j96edknj":{name:"Raszta55",points:0,maps:[]},"8qr4erqj":{name:"xKubSon",points:0,maps:[]},"8grg39yx":{name:"uZer",points:0,maps:[]},"j26qnmnx":{name:"MadTrackEnthusiast",points:0,maps:[]},"18q0nq7j":{name:"luigi100",points:0,maps:[]},"jo267w78":{name:"Austinator06302012",points:0,maps:[]},"8enpdr28":{name:"Drzwi_Dembowe",points:0,maps:[]}};
     const loadmuchdata = function(todo) {
       return new Promise(function(resolve, reject) {
         fetch('https://www.speedrun.com/api/v1/runs?game=369p8x81&status=verified&max=200&offset='+(200*(todo-1))).then(r => r.json()).then(obj => {
@@ -113,6 +113,7 @@ bot.on('message', (message) => {
             if (i.runs.length) {
               var base = i.category == "z27l9wzd" ? 15 : names[i.category].includes("Full game") ? 40 : 30;
               var cattmp = i.category == "z27l9wzd" ? i.level : i.category;
+              var mul = i.category == "z27l9wzd" ? 1 : i.category == "5dw7vnnk" ? 1.6 : i.category == "z27l91zd" ? 2.1 : 2;
               if (cattmp == "kwj3kezd") base += 15; // Adrenaline
               if (cattmp == "kwj3k31d") base += 12; // My Way
               if (cattmp == "y9mymp19") base += 8;  // Rock and Race
@@ -120,9 +121,8 @@ bot.on('message', (message) => {
               if (cattmp == "xd4n53pw" || cattmp == "owolm2yw" || cattmp == "rdnrmpvw") base += 3; // Mona racer, Gravity zero, On the rocks (long races)
               if (cattmp == "xd16z069" || cattmp == "xd16z569" || cattmp == "y9myme19" || cattmp == "kwj3kyzd" || cattmp == "ewpe2qz9" || cattmp == "owolmkyw" || cattmp == "z98eojl9" || cattmp == "z98eo8l9" || cattmp == "o9xkyl1w") base -= 2.5; // Bowling etc (unsure categories 2)
               if (cattmp == "nwlnmgrd") base -= 4;  // Training day
-              if (cattmp == "59236ord" || cattmp == "29vj8yn9") base -= 5; // From the sky, Toygara falls (extremely random, times very close to each other)
+              if (cattmp == "59236ord" || cattmp == "29vj8yn9") {base -= 5; mul *= 0.9;} // From the sky, Toygara falls (extremely random, times very close to each other)
               if (cattmp == "xd4n56pw") base -= 7; // My balloons (just unfair)
-              var mul = i.category == "z27l9wzd" ? 1 : i.category == "5dw7vnnk" ? 1.6 : i.category == "z27l91zd" ? 2.1 : 2;
               i.runs.forEach(j => {
                 var pts = 0;
                 var playertmp = j.run.players[0].rel == "user" ? j.run.players[0].id : j.run.players[0].name;
@@ -152,7 +152,7 @@ bot.on('message', (message) => {
           var s;
           toshow.forEach((u, i) => {        
             if (i < 4) s = "";
-            if (users[u].points > 5) {
+            if (users[u].points >= 10) {
             s += "**\n" + users[u].name.toUpperCase() + "\n**";
               users[u].maps.forEach(m => {
                 if (m.points > 0) s += m.name + " (" + m.place + "): **" + m.points.toFixed(2) + "**\n";
